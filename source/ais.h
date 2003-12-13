775c12b0560678a10ddffa494d6b37a8a40598d1
geomannl <>
2003-12-13 14:32:23 +0000

    net-playability, extra keys support

--- source/ais.h
+++ source/ais.h
@@ -27,6 +27,7 @@ class ControlHuman : public Control {
 	int next;
 	int prev;
 	int closest;
+	int extra1, extra2, extra3, extra4, extra5, extra6;
 	public:
 	virtual void load(const char* inifile, const char* inisection);
 	virtual void save(const char* inifile, const char* inisection);
