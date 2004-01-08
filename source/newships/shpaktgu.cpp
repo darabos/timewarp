52c95f6b9c1e9692956a22b0fde51ad4f80d461f
geomannl <>
2004-01-08 22:22:45 +0000

    adding resurrected ships

---
+++ source/newships/shpaktgu.cpp
@@ -0,0 +1,269 @@
+#include "../ship.h"
+#include "../melee/mshot.h"
+REGISTER_FILE
+#include <string.h>
+
+class AktunComSat : public SpaceObject {
+	int frame;
+	Ship *ship;
+	
+	double lRange;
+	int    lDamage;
+	int    lFrames;
+	int    lRechargeRate;
+	int    lRecharge;
+	int    lColor;
+	int    armour;
+	
+  public:
+	  AktunComSat(double oangle, double orange, int odamage, int oframes,
+		  int orechargerate, int ocolor, int oarmour, Ship *oship,
+		  SpaceSprite *osprite);
+	  
+	  virtual void calculate();
+	  virtual int handle_damage(SpaceLocation* source, double normal, double direct);
+	  //virtual void handle_damage(SpaceLocation *source);
+	  virtual int canCollide(SpaceLocation *other);
+	  virtual void death();
+};
+
+class AktunGunner : public Ship {
+	int    weaponColor;
+	double weaponRange1, weaponRange2, weaponDrain2;
+	int    weaponDamage;
+	
+	double specialRange;
+	double specialVelocity;
+	int    specialDamage;
+	int    specialArmour;
+	
+	double extraRange;
+	int    extraDamage;
+	int    extraFrames;
+	int    extraRechargeRate;
+	int    extraColor;
+	int    extraArmour;
+	int    extraDrain;
+	
+public:
+	AktunGunner(Vector2 opos, double shipAngle, ShipData *shipData, int shipCollideFlag);
+	
+	int num_ComSats;
+	int max_ComSats;
+	AktunComSat **ComSat;
+	
+	virtual int activate_weapon();
+	virtual int activate_special();
+	
+};
+
+class AktunLaser : public Laser {
+	Ship *owner;
+	
+public:
+	AktunLaser(SpaceLocation *creator, double langle, int lweaponColor, double lrange, int ldamage, int lfcount,
+		SpaceLocation *opos, Vector2 relpos, Ship *lowner);
+	
+	virtual void inflict_damage(SpaceObject *other);
+};
+
+AktunGunner::AktunGunner(Vector2 opos, double shipAngle, ShipData *shipData, int shipCollideFlag) :
+Ship(opos, shipAngle, shipData, shipCollideFlag)
+
+{
+	weaponColor  = get_config_int("Weapon", "Color", 0);
+	weaponRange1  = scale_range(get_config_float("Weapon", "Range1", 0));
+	weaponDamage = get_config_int("Weapon", "Damage", 0);
+
+	weaponDrain2 = get_config_float("Weapon", "WeaponDrain2", 0);
+	weaponRange2 = scale_range(get_config_float("Weapon", "Range2", 0));
+	
+	specialRange         = scale_range(get_config_float("Special", "Range", 0));
+	specialVelocity      = scale_velocity(get_config_float("Special", "Velocity", 0));
+	specialDamage        = get_config_int("Special", "Damage", 0);
+	specialArmour        = get_config_int("Special", "Armour", 0);
+	
+	extraRange        = scale_range(get_config_float("Extra", "Range", 0));
+	extraFrames       = get_config_int("Extra", "Frames", 0);
+	extraDamage       = get_config_int("Extra", "Damage", 0);
+	extraDrain        = get_config_int("Extra", "Drain", 0);
+	extraRechargeRate = get_config_int("Extra", "RechargeRate", 0);
+	extraColor        = get_config_int("Extra", "Color", 0);
+	extraArmour       = get_config_int("Extra", "Armour", 0);
+	
+	max_ComSats = get_config_int( "Extra", "Number", 0);
+	num_ComSats = 0;
+	ComSat = new AktunComSat*[max_ComSats];
+	for (int i = 0; i < max_ComSats; i += 1) {
+		ComSat[i] = NULL;
+	}
+	
+}
+
+int AktunGunner::activate_weapon()
+{
+	if(fire_special)
+		return(FALSE);
+
+	// the simplest weapon
+	game->add(new AktunLaser(this, angle,
+		weaponColor, weaponRange1, weaponDamage, weapon_rate,
+		this, Vector2(-10, 8), this));
+
+	// the more expensive weapon: test if there's enough energy left after the first
+	// drain, then subtract the extra drain (the default drain is subracted earlier).
+	if (batt >= weapon_drain + weaponDrain2)
+	{
+		game->add(new AktunLaser(this, angle,
+			weaponColor, weaponRange2, weaponDamage, weapon_rate,
+			this, Vector2(10, 8), this));
+		batt -= weaponDrain2;
+	}
+	return(TRUE);
+}
+
+int AktunGunner::activate_special()
+{
+//	if ((fire_weapon) && (batt >= extraDrain))
+//	{
+		if (num_ComSats == max_ComSats) {
+			num_ComSats -= 1;
+			ComSat[0]->state = 0;
+			memcpy(&ComSat[0], &ComSat[1], sizeof(AktunComSat*) * num_ComSats);
+			ComSat[num_ComSats] = NULL;
+		}
+		AktunComSat *tmp = new AktunComSat(0.0, extraRange,
+			extraDamage, extraFrames, extraRechargeRate, extraColor, extraArmour, this,
+			data->spriteExtra);
+		ComSat[num_ComSats] = tmp;
+		num_ComSats += 1;
+//		batt -= extraDrain;
+		game->add( tmp );
+//		return(FALSE);
+//	}
+
+		/*
+	game->add(new Missile(this,
+		Vector2(0.0, size.y / 2.0), angle, specialVelocity, specialDamage, specialRange,
+		specialArmour, this, data->spriteSpecial) );
+		*/
+	return(TRUE);
+}
+
+
+AktunLaser::AktunLaser(SpaceLocation *creator, double langle, int lweaponColor, double lrange, int ldamage,
+					   int lfcount, SpaceLocation *opos, Vector2 relpos, Ship *lowner) :
+Laser(creator, langle, pallete_color[lweaponColor], lrange,
+	  ldamage, lfcount, opos, relpos, true),
+	  owner(lowner)
+{
+}
+
+void AktunLaser::inflict_damage(SpaceObject *other)
+{
+	sound.stop(owner->data->sampleWeapon[0]);
+	Laser::inflict_damage(other);
+	sound.stop((SAMPLE *)(melee[1].dat));
+	sound.play(owner->data->sampleWeapon[1]);
+}
+
+AktunComSat::AktunComSat(double oangle, double orange, int odamage,
+						 int oframes, int orechargerate, int ocolor, int oarmour, Ship *oship,
+						 SpaceSprite *osprite) :
+SpaceObject(oship, 0, 0, osprite),
+ship(oship),
+lRange(orange),
+lDamage(odamage),
+lFrames(oframes),
+lRechargeRate(orechargerate),
+lRecharge(0),
+lColor(ocolor),
+armour(oarmour)
+{
+	layer = LAYER_SPECIAL;
+	collide_flag_anyone = ALL_LAYERS - bit(LAYER_CBODIES);
+	angle = oangle;
+	pos = ship->normal_pos();
+	if(!(ship && ship->exists())) state = 0;
+}
+
+void AktunComSat::calculate() {
+	
+	if(!(ship && ship->exists())) {
+		state = 0;
+		return;
+	}
+	
+	SpaceObject::calculate();
+
+	sprite_index++;
+	if(sprite_index == 40)
+		sprite_index = 0;
+	
+	if(lRecharge > 0) {
+		lRecharge -= frame_time;
+		return;
+	}
+	
+	vel *= 1 - .0005 * frame_time;
+	
+	if (magnitude_sqr(vel) < 0.05 * 0.05) {
+		vel = 0;
+	}
+	
+	Query q;
+	for (q.begin(this, OBJECT_LAYERS &~ bit(LAYER_CBODIES), lRange); q.currento; q.next()) {
+		if (!q.currento->isInvisible() && !q.currento->sameTeam(this)) {
+			SpaceLocation *l;
+			l = new PointLaser(this, pallete_color[lColor], 1, lFrames, 
+				this, q.currento, 0);
+			game->add(l);
+			if (l->exists()) {
+				sound.play(ship->data->sampleExtra[0]);
+				lRecharge += lRechargeRate;
+				break;
+			}
+			else l->state = 0;
+		}
+	}
+	return;
+}
+
+int AktunComSat::canCollide(SpaceLocation *other) {
+	return SpaceObject::canCollide(other);
+}
+
+int AktunComSat::handle_damage(SpaceLocation* source, double normal, double direct)
+{
+	double tot;
+	tot = normal+direct;
+	if ( tot > 0 ) {
+		armour -= tot;
+		
+		if(armour <= 0) {
+			armour = 0;
+			state = 0;
+			game->add(new Animation(this, pos,
+				meleedata.kaboomSprite, 0, KABOOM_FRAMES, 50, LAYER_EXPLOSIONS));
+			sound.stop(data->sampleExtra[0]);
+			sound.play(data->sampleExtra[0]);
+		}
+	}
+	return 1;
+}
+
+void AktunComSat::death() {
+	for (int i = 0; i < ((AktunGunner*)ship)->num_ComSats; i++ ) {
+		if (((AktunGunner*)ship)->ComSat[i] == this) {
+			((AktunGunner*)ship)->ComSat[i] = NULL;
+			((AktunGunner*)ship)->num_ComSats -= 1;
+			memmove(&((AktunGunner*)ship)->ComSat[i], &((AktunGunner*)ship)->ComSat[i+1], 
+				(((AktunGunner*)ship)->num_ComSats-i) * sizeof(AktunComSat*));
+			return;
+		}
+	}
+}
+
+
+
+REGISTER_SHIP(AktunGunner)
---
+++ source/newships/shptauto.cpp
@@ -0,0 +1,221 @@
+#include "../ship.h"
+#include "../melee/mview.h"
+
+REGISTER_FILE
+
+class TauTor : public Ship
+{
+  double        weaponRange;
+  double        weaponVelocity;
+  int           weaponDamage;
+  int           weaponArmour;
+  double        specialRange;
+  double        specialVelocity;
+  int           specialDamage;
+  int           specialArmour;
+  bool          TauTor_state;
+  bool          can_switch;
+  double        accel_boost, speed_boost;
+  int           extra_drain, extra_rate;
+  int           switch_drain;
+  int           extra_recharge;
+  double        engine_drain, residual_drain;
+  int           transition_time, transition_count;
+
+  public:
+  
+  TauTor     (Vector2 opos, double shipAngle,
+                ShipData *shipData, unsigned int code);
+
+  virtual  int  activate_weapon();
+  virtual void  calculate_fire_special();
+  virtual void  calculate();
+  virtual void  calculate_thrust();
+  virtual void  calculate_hotspots();
+};
+
+class TauTorTwister : public AnimatedShot
+{
+  // SAMPLE* sample_midd;
+  
+  public:
+  
+  TauTorTwister(Vector2 opos, double oangle, double ov, int odamage,
+                  double orange, int oarmour, SpaceLocation *creator, SpaceLocation *lpos,
+                  SpaceSprite *osprite, int ofcount, int ofsize, double relativity = 0);
+
+};
+
+class TauTorPlasma : public AnimatedShot
+{
+  public:
+  TauTorPlasma (Vector2 opos, double oangle, double ov, int odamage,
+                  double orange, int oarmour, SpaceLocation *creator, SpaceLocation *lpos,
+                  SpaceSprite *osprite, int ofcount, int ofsize, double relativity = 0);
+  
+};
+
+
+
+
+TauTor::TauTor(Vector2 opos, double shipAngle,
+                   ShipData *shipData, unsigned int code) :
+        Ship(opos, shipAngle, shipData, code)
+
+{
+        weaponRange     = scale_range(get_config_float("Weapon", "Range", 0));
+        weaponVelocity  = scale_velocity(get_config_float("Weapon", "Velocity", 0));
+        weaponDamage    = get_config_int("Weapon", "Damage", 0);
+        weaponArmour    = get_config_int("Weapon", "Armour", 0);
+
+        specialRange    = scale_range(get_config_float("Special", "Range", 0));
+        specialVelocity = scale_velocity(get_config_float("Special", "Velocity", 0));
+        specialDamage   = get_config_int("Special", "Damage", 0);
+        specialArmour   = get_config_int("Special", "Armour", 0);
+
+        switch_drain    = get_config_int("Extra", "StartDrain", 1);
+        engine_drain    = get_config_float("Extra", "EngineDrain", 0)/1000.0;
+        accel_boost     = get_config_float("Extra", "AccelBoost", 1.0);
+        speed_boost     = get_config_float("Extra", "SpeedBoost", 1.0);
+        transition_time = get_config_int("Extra", "TransitionTime", 1);
+        transition_count= 0;
+
+        TauTor_state  = false;
+        can_switch      = true;
+
+        extra_drain     = weapon_drain;
+        extra_rate      = weapon_rate;
+        extra_recharge  = recharge_amount;
+
+        residual_drain  = 0.0;
+}
+
+int TauTor::activate_weapon()
+{
+        if (transition_count > 0) return false;
+
+        if (TauTor_state) {
+                add(new TauTorPlasma(Vector2(14.0, 24.0), angle,
+					vel.magnitude() + specialVelocity, specialDamage,
+                                  specialRange, specialArmour, this, this, data->spriteSpecial, 6, 50));
+                add(new TauTorPlasma(Vector2(-14.0, 24.0), angle,
+					vel.magnitude() + specialVelocity, specialDamage,
+                                  specialRange, specialArmour, this, this, data->spriteSpecial, 6, 50));
+                weapon_sample = 0; }
+        else {  
+				add(new TauTorTwister(Vector2(0.0, 24.0), angle, weaponVelocity, weaponDamage,
+                                  weaponRange, weaponArmour, this, this, data->spriteWeapon, 6, 50));
+                weapon_sample = 1; }
+        return(TRUE);
+}
+
+void TauTor::calculate_fire_special()
+{
+        special_low = false;
+        if (fire_special && can_switch && (transition_count <=0)) {
+                can_switch = false;
+                if (TauTor_state) {
+                        play_sound(data->sampleExtra[1]);
+                        residual_drain = 0.0;
+                        recharge_amount = extra_recharge;
+                        weapon_rate  = extra_rate;
+                        weapon_drain = extra_drain;
+                        sprite = data->spriteShip;
+                        TauTor_state = false; }
+                else    if (batt > switch_drain) {
+                                transition_count = transition_time;
+                                residual_drain = 0.0;
+                                recharge_amount = 0;
+                                play_sound(data->sampleExtra[0]);
+                                weapon_rate  = special_rate;
+                                weapon_drain = special_drain; }
+                        else    special_low = true; }
+}
+
+void TauTor::calculate()
+{
+		
+        if (transition_count > 0) {
+                if ((transition_count -= frame_time) <= 0) {
+                        sprite = data->spriteExtraExplosion;
+                        TauTor_state = true; }
+                residual_drain += switch_drain * frame_time / (double)transition_time; }
+        int a = (int)floor(residual_drain);
+        batt -= a;
+        residual_drain -= a;
+        if (batt < 0) batt = 0;
+
+        if (TauTor_state && (batt == 0)) {
+                play_sound(data->sampleExtra[1]);
+                residual_drain = 0.0;
+                recharge_amount = extra_recharge;
+                weapon_rate  = extra_rate;
+                weapon_drain = extra_drain;
+                sprite = data->spriteShip;
+                TauTor_state = false; }
+        
+        Ship::calculate();
+        if (!fire_special) can_switch = true;
+}
+
+void TauTor::calculate_thrust()
+{
+
+        if (thrust && (transition_count <=0)) {
+                if (TauTor_state && (batt > 0)) {
+                        residual_drain += engine_drain * frame_time;
+                        accelerate(this, angle, accel_rate * frame_time * accel_boost, speed_max * speed_boost); }
+                else    accelerate(this, angle, accel_rate * frame_time, speed_max); }
+}
+
+void TauTor::calculate_hotspots ()
+{
+	if((thrust) && (hotspot_frame <= 0) && (transition_count <=0)) {
+                if (TauTor_state && (batt > 0))
+			add(new Animation(this, 
+					  //normal_x() - (cos(angle * ANGLE_RATIO) * 21),
+					  //normal_y() - (sin(angle * ANGLE_RATIO) * 21),
+					  normal_pos() - 21 * unit_vector(angle),
+					  data->spriteExtra, 0, 12, time_ratio, LAYER_HOTSPOTS));
+		else	add(new Animation(this, 
+					  //normal_x() - (cos(angle * ANGLE_RATIO) * 21),
+					  //normal_y() - (sin(angle * ANGLE_RATIO) * 21),
+					  normal_pos() - 21 * unit_vector(angle),
+					  meleedata.hotspotSprite, 0, HOTSPOT_FRAMES, time_ratio, LAYER_HOTSPOTS));
+		hotspot_frame += hotspot_rate; }
+	if (hotspot_frame > 0) hotspot_frame -= frame_time;
+	return;
+}
+
+
+TauTorTwister::TauTorTwister(Vector2 opos, double oangle, double ov, int odamage,
+                                 double orange, int oarmour, SpaceLocation *creator, SpaceLocation *lpos,
+                                 SpaceSprite *osprite, int ofcount, int ofsize, double relativity) :
+        AnimatedShot(creator, opos, oangle, ov, odamage, orange, oarmour,
+        lpos, osprite, ofcount, ofsize, relativity)
+{
+        explosionSprite     = data->spriteWeaponExplosion;
+        explosionFrameCount = 12;
+        explosionFrameSize  = 50;
+        //sample_midd         = data->sampleSpecial[0];
+}
+
+
+TauTorPlasma::TauTorPlasma(Vector2 opos, double oangle, double ov, int odamage,
+                               double orange, int oarmour, SpaceLocation *creator, SpaceLocation *lpos,
+                               SpaceSprite *osprite, int ofcount, int ofsize, double relativity) :
+        AnimatedShot(creator, opos, oangle, ov, odamage, orange, oarmour,
+        lpos, osprite, ofcount, ofsize, relativity)
+{
+        explosionSprite     = data->spriteSpecialExplosion;
+        explosionFrameCount = 12;
+        explosionFrameSize  = 50;
+
+}
+
+
+
+
+
+REGISTER_SHIP(TauTor)
+
