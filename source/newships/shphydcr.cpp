97ffb91217f4bd93b3b2eef5269465fa7c918123
geomannl <>
2004-03-26 16:52:55 +0000

    hydrovar fighter fix

--- source/newships/shphydcr.cpp
+++ source/newships/shphydcr.cpp
@@ -289,7 +289,7 @@ int HydrovarCruiser::activate_special()
 //    F->range			= 9999999;
     F->numberOfShots	= iround(specialNumberOfShots);
 	if (specialFiringTakesFuel && specialFuelFull)
-		F->shot_fuel_cost	= iround(specialFuelFull - specialFuelCritical / specialNumberOfShots);
+		F->shot_fuel_cost	= iround((specialFuelFull - specialFuelCritical) / specialNumberOfShots);
 	else
 		F->shot_fuel_cost	= 0;
 	F->fuel					= specialFuelFull;
